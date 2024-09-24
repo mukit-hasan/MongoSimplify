79cfb6fcbe948564ad54e1281e719e602265fcb8
Mukit Hasan <mukithasan58@gmail.com>
2024-09-24 23:38:18 +0600

    updating license and setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+
+DESCRIPTION = 'PymongoModel is a versatile Python library for easy and efficient MongoDB interaction.'
+
 setup(
     name="PyMongoModel",
     version="0.5.0",
     author="Mukit Hasan",
     author_email="mukithasan58@gmail.com",
-    description="A Python library for MongoDB integration with Django-like models",
+    description=DESCRIPTION,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_library",
+    url="https://github.com/mukit-hasan/PymongoModel",
     packages=find_packages(),
     install_requires=[
         "pymongo",
